c84ac71fb5e3e69bda590f0f8c8f25dff788d056
JULIO SENA <gm.sena@hotmail.com>
2024-07-28 10:55:27 -0300

    laços de repetição

--- condicionais/script.js
+++ condicionais/script.js
@@ -9,3 +9,30 @@ if (idade >= 18) {
 }
 
 /* operador ternário*/ 
+
+let nomo ="fulano"; 
+let classe = "mago";
+let nivel = 10;
+let vida = 0;
+let mana = 0;
+switch (classe) {
+    case "guerreiro":
+    case "paladino":
+        vida = 100 * nivel;
+        mana = 50 * nivel;
+        
+        break;
+  case "mago":
+    vida = 50 * nivel;
+    mana = 100 * nivel;
+    break 
+    default:
+        vida = 50 * nivel;
+        mana = 20 * nivel
+        break;
+}
+console.log(nomo);
+console.log(classe);
+console.log(nivel);
+console.log(vida);
+console.log(mana);
---
+++ laçosRepeticao/script.js
@@ -0,0 +1,12 @@
+let contador = 1;
+
+while(contador <= 10){
+    console.log(contador);
+    contador = contador + 1;
+}
+
+
+for(let matematico = 1; matematico <= 10; matematico++){
+    console.log(matematico);
+
+}
