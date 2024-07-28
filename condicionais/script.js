6d0aa586087f880f853a35d7067971b5f7cd0f5a
JULIO SENA <gm.sena@hotmail.com>
2024-07-28 10:12:26 -0300

    javascrit

---
+++ condicionais/script.js
@@ -0,0 +1,11 @@
+let nome = "João";
+let idade = 18;
+if (idade >= 18) {
+    console.log("você é MAIOR DE IDADE");
+} else if( idade === 18){
+ console.log("você acabou de viarar MAIOR de idade");
+} else {
+    console.log("você é MENOR de idade");
+}
+
+/* operador ternário*/ 
