06bc20bf63ea6de9b1f6a65892e39341f4e2a1e7
JULIO SENA <gm.sena@hotmail.com>
2024-08-05 14:04:27 -0300

    Objetos

---
+++ Objeto/script.js
@@ -0,0 +1,2 @@
+name : ''
+nivel
---
+++ Slice - splace/script.js
@@ -0,0 +1,14 @@
+let list = [
+    'Bonieky Lacerda',
+    'Pedro Fulano',
+    'João da Silva',
+    'Gabriela Duarte'
+   ];
+
+ let newList = list.slice(0, 4)
+ console.log(newList);
+
+
+ list.splice(0, 1)
+
+ console.log(list);
