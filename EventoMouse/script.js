d3d2848f3d808057aa39df47a5eefe84b610dba6
JULIO SENA <gm.sena@hotmail.com>
2024-08-09 17:16:30 -0300

    Eventos

---
+++ EventoMouse/script.js
@@ -0,0 +1,18 @@
+let btn  = document.querySelector( 'button');
+
+btn.addEventListener('click',() => {
+    console.log('clicou');
+})
+btn.addEventListener('mousedown', () =>{
+    console.log('Mouse Down');
+})
+btn.addEventListener('mouseup',() =>{
+    console.log('Mouse Up');
+})
+
+btn.addEventListener('mouseenter',() =>{
+    console.log('MOuse Enter');
+})
+btn.addEventListener('mouseleave', () =>{
+    console.log('Mouse Leave');
+})
---
+++ EventosTeclado/script.js
@@ -0,0 +1,7 @@
+function apertou(event) {
+
+    if (event.key.toLowerCase() === 'enter') {
+        
+    }
+    console.log('Eita! Apertou o enter' );
+}
