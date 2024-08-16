49618319fa270995a505037e05311d031f5cb80d
JULIO SENA <gm.sena@hotmail.com>
2024-08-16 10:29:55 -0300

    GET POST PUT DELETE

---
+++ APIs/Introdução Fetch/script.js
@@ -0,0 +1,51 @@
+ /*
+ O QUE SÃO APIS?
+ 
+ APPLICATION PROGRAMMING INTERFACCE
+ INTERFACE DE PROGRAMAÇÃO DA APLICAÇÃO
+
+
+
+ APP INSTRAGAM:
+ -verificar se você está logado.
+ -Acessa um URL no servidor do instragan
+ -manda o cara logado para a HOME .
+ - Acesso uma URL nom servidor do instragen
+ */
+
+/* https://jsonplaceholder.typicode.com/users */
+
+function loadUsers() {
+    let url = 'https://jsonplaceholder.typicode.com/users'
+    fetch(url)
+        .then((response)=>{
+            return response.json()
+    }).then((json)=>{
+        console.log(json.length);
+    })
+}
+//Metodo GET, POST
+
+function loadUsers() {
+    let url = 'https://jsonplaceholder.typicode.com/users'
+    fetch(url,{
+        method:'POST',
+        body: JSON.stringify({
+            title: 'Teste',
+            body: 'corpo de teste',
+            userId: '10'
+        })
+    })
+        .then((response)=>{
+            return response.json()
+    }).then((json)=>{
+        console.log(json.length);
+    })
+}
+// GET, POST, PUT e DELETE
+
+// GET = Quando quero pegar uma informação no servidor
+// Post = Quando quero inserir uma informação no servidor
+// PUT = Quando quero atualizar uma informação no servidor
+// DELETE = Quando quero excluir uma informação no servidor
+//
